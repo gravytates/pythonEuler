0b2be64758e8bc69358ed701030d313cab8446e8
Grady L Shelton <gradyish@gmail.com>
2017-09-30 16:27:37 -0700

    sum even fibonacci numbers up to million

--- pythonProblem.py
+++ pythonProblem.py
@@ -1,14 +1,28 @@
-# Sum of multiples of 3 and 5 up to 1000
-
 from functools import reduce
-total = []
-for i in range(1000):
-    if i % 3 == 0:
-        total.append(i)
-    elif i % 5 == 0:
-        total.append(i)
-    else:
-        continue
 
-grandTotal = reduce((lambda x, y: x + y), total)
-print(grandTotal)
+# Sum of multiples of 3 and 5 up to 1000
+# total = []
+# for i in range(1000):
+#     if i % 3 == 0:
+#         total.append(i)
+#     elif i % 5 == 0:
+#         total.append(i)
+#     else:
+#         continue
+#
+# answer = reduce((lambda x, y: x + y), total)
+
+# Sum of even fibonacci numbers up to 4 million
+
+fibList = []
+a,b = 0,1
+while b < 4000000:
+    if a % 2 == 0:
+        fibList.append(a)
+    if b % 2 == 0:
+        fibList.append(b)
+    a,b = b, a+b
+
+answer = reduce((lambda x,y: x + y), set(fibList))
+
+print(answer)
